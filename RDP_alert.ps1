92c97cb5ad11ed1e9eec36807d388a4f1b95d3d5
Fuxnet <35876715+Fuxnet@users.noreply.github.com>
2018-01-27 19:45:18 +0000

    Powershell Repo

---
+++ RDP_alert.ps1
@@ -0,0 +1,25 @@
+﻿$rdpinfo = Get-WinEvent -LogName Microsoft-Windows-TerminalServices-LocalSessionManager/Operational | Where {$_.Id -eq "25"}| select -ExpandProperty Message -First 1
+
+
+$email = "XXXX@test.com" #Add sender email here 
+
+$sendto = "XXXX@test.com" #Add email recipient
+ 
+$pass = "XXX" #Email Password for sender 
+$port = "587"
+ 
+$smtpServer = "smtp.XXX.com" #Add SMTP server from sender
+
+$msg = new-object Net.Mail.MailMessage 
+$smtp = new-object Net.Mail.SmtpClient($smtpServer, $port) #also ref here
+$smtp.EnableSsl = $true 
+$msg.From = "$email"  
+$msg.To.Add("$sendto") 
+$msg.BodyEncoding = [system.Text.Encoding]::ASCII 
+$msg.SubjectEncoding = [system.Text.Encoding]::ASCII
+$msg.IsBodyHTML = $true  
+$msg.Subject = "RDP Login Alert" 
+$msg.Body = "<p>$rdpinfo</p>"
+$SMTP.Credentials = New-Object System.Net.NetworkCredential("$email", "$pass"); 
+$smtp.Send($msg)
+exit
